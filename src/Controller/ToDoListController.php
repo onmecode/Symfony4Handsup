80976b9796b8722239a8f54622c0e6e654403d55
vagrant <vagrant@ubuntu1804.localdomain>
2019-06-25 15:57:07 -0700

    starting project

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    /**
+     * @Route("/to/do/list", name="to_do_list")
+     */
+    public function index()
+    {
+        return $this->render('to_do_list/index.html.twig', [
+            'controller_name' => 'ToDoListController',
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create()
+    {
+        exit('to do: create a new task!');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch-status")
+     */
+    public function switchStatus($id)
+    {
+        exit('to do:switch status of task'. $id);
+    }
+
+}
